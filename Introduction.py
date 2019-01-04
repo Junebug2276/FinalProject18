6a8101d92384a65ee1fe7d3e88235c5a0f070e84
Jose Montanez <Junebug2276@4261811f904c>
2019-01-04 17:31:31 +0000

    This is the player introduction for the game!

---
+++ Introduction.py
@@ -0,0 +1,11 @@
+print("Welcome to the fight for your freedom! We don't have much time, the breakout is already in progress and we need to get you out of here soon before they contain the stragglers. It's going to be rough, I hope you're ready for a fight.")
+
+print("*Explosions and suppressed yelling in the distance.*")
+
+player_name = input("What's your name, degenerate?!")
+
+def inmate_introduction():
+    print(f"Okay, so you are {player_name}, right? I hope you're ready to move, because there isn't much time until our opportunity is gone. You have to fight your way through the facility if you have any hopes of getting out. I know you may have messed up, but that doesn't mean you should stay here forever.")
+
+inmate_introduction()
+
