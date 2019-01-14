2302bf9934673f58de39017d14bb1e35028ef50b
Jose Montanez <Junebug2276@4261811f904c>
2019-01-14 18:21:48 +0000

    Updated game objectives

--- Introduction.py
+++ Introduction.py
@@ -5,7 +5,24 @@ print("*Explosions and suppressed yelling in the distance.*")
 player_name = input("What's your name, degenerate?!")
 
 def inmate_introduction():
-    print(f"Okay, so you are {player_name}, right? I hope you're ready to move, because there isn't much time until our opportunity is gone. You have to fight your way through the facility if you have any hopes of getting out. I know you may have messed up, but that doesn't mean you should stay here forever.")
+    print(f"Okay, so you're {player_name}, right? I hope you're ready to move, because there isn't much time until our opportunity is gone. You have to fight your way through the facility if you have any hopes of getting out. I know you may have messed up, but that doesn't mean you should stay here forever.")
 
 inmate_introduction()
+
+print("First thing's first, you need to figure out where you need to get. There are four blocks, one connected to another. There's The Infirmary, The Cafeteria, The Cells, and The Warden's Office. Hopefully you don't run into too much trouble on the way out. The Warden's Office is where the exit is located. Get through there, and you're home free. Good Luck!")
+
+start_mission == input("Ready to start?")
+
+if start_mission == yes:
+    print("Alright, let's do this!")
+
+print(f"You look around the small, dark cell. Nothing is really visible, but something shiny by the cell door seems to catch your eye.")
+
+if user _choice = yes:
+    print("There is a screwdriver on the floor. This could prove useful for the future.")
+    elif print("You better hope that wan't anything impoortant.")
+
+else:
+    print("Sorry, it's too late to turn back now. Have fun!")
+
 
